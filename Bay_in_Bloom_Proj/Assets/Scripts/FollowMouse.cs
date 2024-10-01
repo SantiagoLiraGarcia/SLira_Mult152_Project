449e357eb4f57e18ef871ff0e6a6dc6c6546e8b7
SantiagoLiraGarcia <124302459+SantiagoLiraGarcia@users.noreply.github.com>
2024-10-01 01:45:34 -0700

    t

---
+++ Bay_in_Bloom_Proj/Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+  public Vector3 screenPosition;
+  public Vector3 worldPosition;
+
+  void Update()
+  {
+	  screenPosition = Input.mousePosition;
+
+	  Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+
+	  if(Physics.Raycast(ray, out RaycastHit hitData))
+	  {
+		  worldPosition = hitData.point;
+	  }
+
+	  transform.position = worldPosition;
+  }
+}
